4eb6e5634d33b026852061666c138cc9b3a8153c
Mufeed Kcp <bitengineguy@gmail.com>
2026-09-21 08:54:42 +0530

    Added leetcode 1280. Students and Examinations

---
+++ Scripts/11.Students-&-Examinations.sql
@@ -0,0 +1,11 @@
+SELECT 
+	students.student_id, 
+	student_name, 
+	subjects.subject_name,
+    COUNT(examinations.subject_name) AS attended_exams
+FROM students
+JOIN subjects
+LEFT JOIN examinations
+	ON students.student_id = examinations.student_id AND subjects.subject_name = examinations.subject_name
+GROUP BY students.student_id, student_name, subjects.subject_name
+ORDER BY students.student_id, subjects.subject_name
